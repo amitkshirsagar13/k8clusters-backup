65e12582a49b245126456bac2c34c6b341094906
Kshirsagar, Amit <amit.kshirsagar@siemens.com>
2019-12-05 19:30:53 +0530

    Update TestApplication.java

--- test/testserver/src/main/java/io/k8clusters/TestApplication.java
+++ test/testserver/src/main/java/io/k8clusters/TestApplication.java
@@ -1,12 +1,18 @@
 package io.k8clusters;
 
+import io.k8clusters.base.service.BaseService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ImportResource;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TestApplication {
     public static void main(String[] args) {
         SpringApplication.run(TestApplication.class, args);
+    }
+
+    @Bean
+    public BaseService baseService() {
+        return new BaseService();
     }
 }
